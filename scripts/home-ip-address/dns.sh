34e817a2e3b538ad123bc99f4e33370743ed675a
Jurijs Kolomijecs <jurikolo@yandex.com>
2021-05-05 22:29:48 +0300

    HOTFIX: revert IP comparison

--- scripts/home-ip-address/dns.sh
+++ scripts/home-ip-address/dns.sh
@@ -34,7 +34,7 @@ export SOURCE_IP=$(http -b POST https://source-ip.jurikolo.name)
 export DNS_IP=$(dig +short +nocookie "$DOMAIN")
 
 # Compare results and update Route53
-if [ "$DNS_IP" = "$SOURCE_IP" ]; then
+if [ "$DNS_IP" != "$SOURCE_IP" ]; then
     echo "Actual IP address is different than in Route53. Going to update Route53 with new IP address...";
     echo "{ \"Changes\": [{ \"Action\" : \"UPSERT\", \"ResourceRecordSet\" : { \"Name\" : \"$DOMAIN\", \"Type\" : \"A\", \"TTL\" : 120, \"ResourceRecords\" : [{\"Value\" : \"$SOURCE_IP\"}] }}]}" > dns.json;
     export HOSTED_ZONE_ID=$(aws --profile "$PROFILE" route53 list-hosted-zones-by-name --dns-name "$ZONE" | jq -r '.HostedZones[0].Id');
